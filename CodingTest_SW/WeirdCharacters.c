c82c33200ae89edd98a3a009985b509a06cc1e79
apf_temp_admin <s2110@e-mirim.hs.kr>
2022-09-17 22:42:13 +0900

    0914 이상한 문자 만들기

---
+++ CodingTest_SW/WeirdCharacters.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(const char* s) {
+    char* answer = (char*)malloc(strlen(s) + 1);
+    int index = 0;
+    strcpy(answer, s);
+    for (int i = 0; i < strlen(answer); i++) {
+        if (answer[i] == ' ') {
+            index = 0;
+        }
+        else if (index % 2 == 0) {
+            answer[i] = toupper(answer[i]);
+            index++;
+        }
+        else if (index % 2 == 1) {
+            answer[i] = tolower(answer[i]);
+            index++;
+        }
+    }
+    return answer;
+}
